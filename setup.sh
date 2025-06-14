ea590a038f845a2c21834b55e002e3b2e817b429
Antônio Paulo <antoniopaulo404ap@gmail.com>
2025-06-14 18:33:40 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,9 +20,22 @@ fi
 if node -v; then
     echo "Node.js já está instalado."
 else
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v-1.40.2/install.sh | bash
-    \. "$HOME/.nvm/nvm.sh"
-    nvm install 21
+    # Descarregar e instalar a nvm:
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
+
+# Em vez de reiniciar a concha ou shell
+\. "$HOME/.nvm/nvm.sh"
+
+# Descarregar e instalar a Node.js:
+nvm install 22
+
+# Consultar a versão da Node.js:
+node -v # Deveria imprimir "v22.16.0".
+nvm current # Deveria imprimir "v22.16.0".
+
+# Consultar a versão da npm:
+npm -v # Deveria imprimir "10.9.2".
+
 fi
 
 #Instalação do Vim-plug
